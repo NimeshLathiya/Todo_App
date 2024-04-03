d0f622e7ba6fc1dad993c4efd67558dbf74a31ec
nimeshlathiya <142136394+NimeshLathiya@users.noreply.github.com>
2024-04-03 07:33:15 +0530

    Update TodoItem.jsx

--- src/Components/TodoItem.jsx
+++ src/Components/TodoItem.jsx
@@ -3,7 +3,7 @@ import { useState } from "react";
 import { useTodo } from "../Contexts/TodoContexts";
 
 function TodoItem({ todo }) {
-  const [isTodoEditable, setIsTodoEditable] = useState();
+  const [isTodoEditable, setIsTodoEditable] = useState(false);
   const [todoMsg, setTodoMsg] = useState(todo.todo);
   const { updateTodo, deleteTodo, toggleComplete } = useTodo();
 
